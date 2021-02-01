740cc30f58ad7eed347bdec350a431268c8acb9f
catteri <caterinascob@gmail.com>
2021-02-01 21:24:42 +0100

    homepage-section slider changed/jquery way ghanged

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,6 @@ function styles() {
 
 function scripts() {
     return src([
-        'node_modules/jquery/dist/jquery.js',
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
