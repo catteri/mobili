fbde6ddb0b740b4b8ef9f46dffdb489fb1aa241c
catteri <caterinascob@gmail.com>
2021-02-09 13:25:16 +0100

    added responsive header

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,10 @@
 $(function () {
 
+   $('.menu__btn').on('click', function() {
+    $('.menu__list').toggleClass('menu__list--active')
+  });
+  
+
   $('.description-tabs__top-item').on('click', function (e) {
     
     e.preventDefault();
